ecf5f38e3f509925799a07e1cd42e34d20e39214
Lucas Delboni <lucas.delboni2@gmail.com>
2020-04-25 12:59:38 -0300

    initial commit with about and contact

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Contact from './section/Contact';
+import About from './section/About';
+
+function App() {
+  return (
+    <div className="App">
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
